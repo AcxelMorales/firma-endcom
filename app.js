b94b994b0c2b0fed17d17991f1404d4e53f669f3
Acxel Morales <acxel@conecta.mx>
2020-05-11 16:04:12 -0500

    Update size

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -11,6 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.svg')));
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
